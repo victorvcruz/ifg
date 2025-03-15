9c51596a18678d4da0694c2063ce8604cd3456e1
Victor Cruz <victor.cruz@vocedm.com.br>
2025-03-15 17:30:16 -0300

    M 4ª Período/BDII/TRABALHO-II.sql

--- 4ª Período/BDII/TRABALHO-II.sql
+++ 4ª Período/BDII/TRABALHO-II.sql
@@ -1,3 +1,5 @@
 -- Victor Hugo Vieira Cruz
 
 --1) Escreva o comando para inserir um novo autor chamado "Milton Nascimento" que tenha composto as músicas de ID 90 e 91
+
+--Script SQL (PostgreSQl) – Valor 0,2:
