f2448ed907e740946e6a213d88b77e2cf0002d30
Victor Cruz <victor.cruz@vocedm.com.br>
2025-03-15 18:09:22 -0300

    M 4ª Período/BDII/TRABALHO-II.sql

--- 4ª Período/BDII/TRABALHO-II.sql
+++ 4ª Período/BDII/TRABALHO-II.sql
@@ -137,6 +137,14 @@ WHERE Dt_Lancamento < '1990-01-01';
 
 db.CD.deleteMany({ Dt_Lancamento: { $lt: new Date('1990-01-01') } });
 
---7) Escreva a operação da álgebra relacional e o comando SQL para listar todas as músicas compostas por
-"Renato Russo".
+--7) Escreva a operação da álgebra relacional e o comando SQL para listar todas as músicas compostas por "Renato Russo".
+
+σ 
+Nome_Autor= 
+′
+ RenatoRusso 
+′
+ 
+​
+ (Autor)
 
