42fb1a5f99243e90db5345f86459b7c0f933af78
Victor Cruz <victor.cruz@vocedm.com.br>
2025-03-15 18:11:30 -0300

    M 4ª Período/BDII/TRABALHO-II.sql

--- 4ª Período/BDII/TRABALHO-II.sql
+++ 4ª Período/BDII/TRABALHO-II.sql
@@ -143,3 +143,10 @@ db.CD.deleteMany({ Dt_Lancamento: { $lt: new Date('1990-01-01') } });
 
 Musicas_de_Renato_Russo=πidMusica(σNome_Autor= 'RenatoRusso'(Autor)⋈Musica_tem_Autor)
 
+π 
+NomeMusica
+​
+ (Musica⋈M 
+u
+ˊ
+ sicas_de_Renato_Russo)
